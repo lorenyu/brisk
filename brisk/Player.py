07192317e391d6a68da4b181a9e46d0158396093
Loren Yu <yuloren@gmail.com>
2013-02-06 23:51:17 -0800

    added functions for player to compute continents, controlled continents, num armies, etc

--- brisk/Player.py
+++ brisk/Player.py
@@ -10,6 +10,26 @@ class Player():
         self.num_reserves = 0
         self.territories = []
 
+    @property
+    def controlled_continents(self):
+        controlled_territories = set(self.territories)
+        continents = set([territory.continent for territory in self.territories])
+        controlled_continents = filter(lambda continent: controlled_territories.issuperset(continent.territories), continents)
+        return controlled_continents
+
+    @property
+    def continents_with_controlled_territory(self):
+        return list(set([territory.continent for territory in self.territories]))
+        
+
+    @property
+    def num_territories_needed_for_extra_base_armies(self):
+        num_territories = len(self.territories)
+        return 3 - (num_territories % 3)
+
+    def territories_needed_for_continent(self, continent):
+        return [territory for territory in continent.territories if territory.player != self]
+
     def update(self, player_status_data, brisk_map):
         self.is_current_turn = player_status_data['current_turn']
         self.is_eliminated = player_status_data['eliminated']
