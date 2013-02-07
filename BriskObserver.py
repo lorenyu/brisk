9ba827f80efb821c2a76ac5a9759d150bb6795a2
Loren Yu <yuloren@gmail.com>
2013-02-06 22:44:37 -0800

    removed files in top directory that were moved.

--- BriskObserver.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class BriskObserver:
-
-    def update(self, brisk):
-        with open('www/temp.html', 'w') as out:
-            out.write(brisk.get_map_svg())
-        os.rename('www/temp.html', 'www/map.svg.html')
