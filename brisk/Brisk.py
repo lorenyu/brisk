4b873041d7ece6593eaad62c8c5d79385dc3a25f
Loren Yu <yuloren@gmail.com>
2013-02-09 19:34:36 -0800

    pass in token to join game

--- brisk/Brisk.py
+++ brisk/Brisk.py
@@ -16,7 +16,7 @@ class Brisk(object):
         return res
 
     def join_game(self, game_id):
-        data = { 'join': True, 'team_name': self.team_name, 'game': game_id, 'no_bot': True }
+        data = { 'join': True, 'team_name': self.team_name, 'game': game_id, 'no_bot': True, 'token': '4f2bb842e325bf5eaca6c98764f1bf5249802e33' }
         res = self.post(self.url_root(), data)
         self.game_id = res['game']
         self.player_id = res['player']
